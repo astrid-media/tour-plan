80a87967b3bf2af6d43173930405b52087f05bec
astrid-media <info@astrid-media.ru>
2021-03-20 13:43:10 +0300

    create: add modal window

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -64,4 +65,22 @@ $('.parallax-window').parallax({imageSrc: 'img/newsletter-bg.jpeg'});
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener('click', function () {
   document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $(".modal__close");
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+function openModal(){
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass("modal__overlay--visible");
+  modalDialog.addClass("modal__dialog--visible");
+}
+function closeModal(){
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+}
 });
