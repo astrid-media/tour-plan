891eda8e45c0b1b0fee274ca7b24dd4a4d974a54
astrid-media <info@astrid-media.ru>
2021-03-24 21:28:13 +0300

    fix: finish fix

--- js/main.js
+++ js/main.js
@@ -27,39 +27,6 @@ const reviewSlider = new Swiper('.reviews-slider', {
   },
   
 });
-
-
-    ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [7.573638, 79.803867],
-            zoom: 12
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-
-        // Создаём макет содержимого.
-        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-        ),
-
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Grand Hilton Hotel',
-         //   balloonContent: 'Это красивая метка'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'img/icon-place.png',
-            // Размеры метки.
-            iconImageSize: [30, 30],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        });
-        myMap.geoObjects
-        .add(myPlacemark);
-});
 $('.parallax-window').parallax({imageSrc: 'img/newsletter-bg.jpeg'});
 
 var menuButton = document.querySelector(".menu-button");
